66561b5492ef5540978457c678049f5dc74a013c
Darkkid819 <darkkid8192004@gmail.com>
2024-04-28 20:00:18 -0400

    working mode7 + tweaked parameters

--- mode7.c
+++ mode7.c
@@ -4,15 +4,23 @@
 #define SCREEN_WIDTH 1920
 #define SCREEN_HEIGHT 1080
 
+typedef struct Frustum {
+    Vector2 Far1;
+    Vector2 Far2;
+    Vector2 Near1;
+    Vector2 Near2;
+} Frustum;
+
 Texture2D texSky;
 Texture2D texMap;
 Image imgMap;
+Frustum frustum;
 
-float fWorldX = 1000.0f;
-float fWorldY = 1000.0f;
+float fWorldX = 0.0f;
+float fWorldY = 0.0f;
 float fWorldA = 0.1f;
 float fNear = 0.005f;
-float fFar = 0.03f; 
+float fFar = 20.0f; 
 float fFoVHalf = PI / 4.0f;
 float fSpeed = 50.0f; 
 
@@ -27,27 +35,39 @@ int main(void) {
     imgMap = LoadImageFromTexture(texMap);
 
     while (!WindowShouldClose()) {
-        if (IsKeyDown(KEY_Q)) fNear += 1.0f * GetFrameTime();
-        if (IsKeyDown(KEY_A)) fNear -= 1.0f * GetFrameTime();
+        if (IsKeyDown(KEY_Q)) fNear += 0.1f * fSpeed * GetFrameTime();
+        if (IsKeyDown(KEY_A)) fNear -= 0.1f * fSpeed * GetFrameTime();
         
-        if (IsKeyDown(KEY_W)) fFar += 1.0f * GetFrameTime();
-        if (IsKeyDown(KEY_S)) fFar -= 1.0f * GetFrameTime();        
+        if (IsKeyDown(KEY_W)) fFar += 0.1f * fSpeed * GetFrameTime();
+        if (IsKeyDown(KEY_S)) fFar -= 0.1f * fSpeed * GetFrameTime();        
 
-        if (IsKeyDown(KEY_Z)) fFoVHalf += 1.0f * GetFrameTime();
-        if (IsKeyDown(KEY_X)) fFoVHalf -= 1.0f * GetFrameTime();
+        if (IsKeyDown(KEY_Z)) fFoVHalf += 0.1f * fSpeed * GetFrameTime();
+        if (IsKeyDown(KEY_X)) fFoVHalf -= 0.1f * fSpeed * GetFrameTime();
 
-        if (IsKeyDown(KEY_RIGHT)) fWorldA -= 1.0f * GetFrameTime();
-        if (IsKeyDown(KEY_LEFT)) fWorldA += 1.0f * GetFrameTime();
+        if (IsKeyDown(KEY_RIGHT)) fWorldA += 1.0f * GetFrameTime();
+        if (IsKeyDown(KEY_LEFT)) fWorldA -= 1.0f * GetFrameTime();
 
         if (IsKeyDown(KEY_UP)) {
             fWorldX += cosf(fWorldA) * fSpeed * GetFrameTime();
-            fWorldY -= sinf(fWorldA) * fSpeed * GetFrameTime();
+            fWorldY += sinf(fWorldA) * fSpeed * GetFrameTime();
         }
 
         if (IsKeyDown(KEY_DOWN)) {
             fWorldX -= cosf(fWorldA) * fSpeed * GetFrameTime();
-            fWorldY += sinf(fWorldA) * fSpeed * GetFrameTime();
+            fWorldY -= sinf(fWorldA) * fSpeed * GetFrameTime();
         }
+
+        frustum.Far1.x = fWorldX + cosf(fWorldA - fFoVHalf) * fFar;
+        frustum.Far1.y = fWorldY + sinf(fWorldA - fFoVHalf) * fFar;
+        
+        frustum.Near1.x = fWorldX + cosf(fWorldA - fFoVHalf) * fNear;
+        frustum.Near1.y = fWorldY + sinf(fWorldA - fFoVHalf) * fNear;
+        
+        frustum.Far2.x = fWorldX + cosf(fWorldA + fFoVHalf) * fFar;
+        frustum.Far2.y = fWorldY + sinf(fWorldA + fFoVHalf) * fFar;
+        
+        frustum.Near2.x = fWorldX + cosf(fWorldA + fFoVHalf) * fNear;
+        frustum.Near2.y = fWorldY + sinf(fWorldA + fFoVHalf) * fNear;
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
@@ -70,28 +90,28 @@ int main(void) {
 }
 
 static void DrawMode7Line(int y) {
-    float fDepth = 1.0f / ((float)(y) / (SCREEN_HEIGHT / 2.0f) * (fFar - fNear) + fNear);
-    
-    Vector2 lineStart = {
-        fWorldX - cosf(fWorldA - fFoVHalf) * fDepth,
-        fWorldY - sinf(fWorldA - fFoVHalf) * fDepth
-    };
+    float fSampleDepth = (float)y / ((float)GetScreenHeight() / 2.0f);
 
-    Vector2 lineEnd = {
-        fWorldX + cosf(fWorldA + fFoVHalf) * fDepth,
-        fWorldY + sinf(fWorldA + fFoVHalf) * fDepth
-    };
+    float fStartX = (frustum.Far1.x - frustum.Near1.x) / (fSampleDepth) + frustum.Near1.x;
+    float fStartY = (frustum.Far1.y - frustum.Near1.y) / (fSampleDepth) + frustum.Near1.y;
+    float fEndX = (frustum.Far2.x - frustum.Near2.x) / (fSampleDepth) + frustum.Near2.x;
+    float fEndY = (frustum.Far2.y - frustum.Near2.y) / (fSampleDepth) + frustum.Near2.y;
 
     for (int x = 0; x < SCREEN_WIDTH; x++) {
-        float fSampleWidth = (float)x / (float)SCREEN_WIDTH;
-        float fSampleX = (lineEnd.x - lineStart.x) * fSampleWidth + lineStart.x;
-        float fSampleY = (lineEnd.y - lineStart.y) * fSampleWidth + lineStart.y;
+        float fSampleWidth = (float)x / (float)GetScreenWidth();
+        float fSampleX = (fEndX - fStartX) * fSampleWidth + fStartX;
+        float fSampleY = (fEndY - fStartY) * fSampleWidth + fStartY;
+
+        // fSampleX = fmod(fSampleX, (texMap.width + texMap.width));
+        // fSampleX = fmod(fSampleX, texMap.width);
+        // fSampleY = fmod(fSampleY, (texMap.height + texMap.height));
+        // fSampleY = fmod(fSampleY, texMap.height);
 
         int texX = ((int)fSampleX % texMap.width + texMap.width) % texMap.width;
         int texY = ((int)fSampleY % texMap.height + texMap.height) % texMap.height;
 
         Color color = GetImageColor(imgMap, texX, texY);
 
-        DrawPixel(x, (SCREEN_HEIGHT / 2) + y, color);
+        DrawPixel(x, (GetScreenHeight() / 2) + y, color);
     }
 }
